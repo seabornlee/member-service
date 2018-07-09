d404e120de392993df3c1e9d6c55b8c8b2ac7254
Seaborn Lee <seabornlee.cn@gmail.com>
2018-07-10 01:56:20 +0800

    Fix flyway issues.

--- src/main/java/com/seabornlee/springboot/memberservice/domain/Member.java
+++ src/main/java/com/seabornlee/springboot/memberservice/domain/Member.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String mobile;
