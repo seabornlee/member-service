c5fd575e0ef81c558ec9a9c69a53e9fc963dd603
Seaborn Lee <seabornlee.cn@gmail.com>
2018-07-18 19:31:39 +0800

    Make Integration test run faster.

--- src/test/java/com/seabornlee/springboot/memberservice/BaseIntegrationTest.java
+++ src/test/java/com/seabornlee/springboot/memberservice/BaseIntegrationTest.java
@@ -7,10 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = BEFORE_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseIntegrationTest {}
