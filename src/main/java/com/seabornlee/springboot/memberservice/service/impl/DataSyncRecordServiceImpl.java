a4264b4ac3a89e75f9b538f45084b4fc5745269a
刘旭辉 <liuxuhui20082000@126.com>
2018-07-27 22:48:00 +0800

    feat: 数据同步记录 service test: 数据同步记录 mapper test: 数据同步记录 service

---
+++ src/main/java/com/seabornlee/springboot/memberservice/service/impl/DataSyncRecordServiceImpl.java
@@ -0,0 +1,45 @@
+package com.seabornlee.springboot.memberservice.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.seabornlee.springboot.memberservice.domain.DataSyncRecord;
+import com.seabornlee.springboot.memberservice.mapper.DataSyncRecordMapper;
+import com.seabornlee.springboot.memberservice.service.IDataSyncRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataSyncRecordServiceImpl implements IDataSyncRecordService {
+
+    @Autowired
+    private DataSyncRecordMapper dataSyncRecordMapper;
+
+    @Override
+    public PageInfo<DataSyncRecord> getListByPage(int page, int size) {
+        page = page>0?page:1;
+        size = size>0?size:10;
+        PageHelper.startPage(page, size,true);
+
+        PageInfo<DataSyncRecord> pageInfo = new PageInfo<>(dataSyncRecordMapper.selectAll());
+
+        return pageInfo;
+    }
+
+    @Override
+    public void saveRecord(DataSyncRecord record) {
+
+        dataSyncRecordMapper.insert(record);
+    }
+
+    @Override
+    public void updateRecord(DataSyncRecord record) {
+
+        dataSyncRecordMapper.updateByPrimaryKey(record);
+    }
+
+    @Override
+    public void deleteRecord(DataSyncRecord record) {
+
+        dataSyncRecordMapper.delete(record);
+    }
+}
---
+++ src/test/java/com/seabornlee/springboot/memberservice/service/impl/DataSyncRecordServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.seabornlee.springboot.memberservice.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageInfo;
+import com.seabornlee.springboot.memberservice.domain.DataSyncRecord;
+import com.seabornlee.springboot.memberservice.service.IDataSyncRecordService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+public class DataSyncRecordServiceImplTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Mock
+    private IDataSyncRecordService dataSyncRecordService;
+
+    @Test
+    public void getListByPage() {
+
+        PageInfo<DataSyncRecord> page = dataSyncRecordService.getListByPage(1,10);
+
+        logger.info(JSON.toJSONString(page));
+
+    }
+}
