bac64b53ddfa936a217832ce9c75d2dcd1cbfb92
liuxuhui <liuxuhui@corp.netease.com>
2018-07-31 17:58:44 +0800

    feat: 创建 sku 库位 数据库表

---
+++ src/main/resources/db/migration/V1__20180731_create_table_warehouse.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `warehouse`;
+CREATE TABLE `warehouse` (
+`id` int UNSIGNED NULL AUTO_INCREMENT,
+`warehouse_no` varchar(50) NULL COMMENT '仓库编号',
+`name` varchar(50) NULL COMMENT '名称',
+`type` varchar(50) NULL,
+`is_master` boolean NULL COMMENT '是否主库',
+`is_default` boolean NULL COMMENT '是否默认',
+`is_position_enabled` boolean NULL COMMENT '是否允许位置',
+`province` int(11) NULL,
+`city` int(11) NULL,
+`district` int(11) NULL,
+`address` varchar(255) NULL COMMENT '详细地址',
+`tenant_id` int(11) NULL COMMENT '租户id',
+PRIMARY KEY (`id`) ,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/main/resources/db/migration/V1__20180731_create_table_warehouse_bin.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `warehouse_bin`;
+CREATE TABLE `warehouse_bin` (
+`id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+`bin_no` varchar(50) NULL COMMENT '编号',
+`name` varchar(50) NULL COMMENT '名称',
+`area_type` varchar(50) NULL COMMENT '区域类型',
+`area_name` varchar(50) NULL COMMENT '区域名',
+`shelf_no` varchar(50) NULL COMMENT '货架编号',
+`is_tmp` boolean NULL COMMENT '是否是临时',
+`is_enabled` boolean NULL DEFAULT false COMMENT '是否禁用',
+`data_source` int NULL DEFAULT 1 COMMENT '数据来源： 1 e精灵',
+`warehouse_id` int NULL COMMENT '仓库id',
+PRIMARY KEY (`id`) ,
+INDEX `warehouse_bin_no_idx` (`bin_no` ASC)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
