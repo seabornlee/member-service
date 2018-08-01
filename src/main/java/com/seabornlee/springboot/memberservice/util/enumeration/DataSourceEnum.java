46a6ce10e1ac1a73da5d73d6cab9ae460e449ab1
liuxuhui <liuxuhui@corp.netease.com>
2018-08-01 17:40:39 +0800

    feat: 增加数据同步任务状态和数据来源枚举类型 refactor: 修改data_sync_record表data_source字段为int类型

---
+++ src/main/java/com/seabornlee/springboot/memberservice/util/enumeration/DataSourceEnum.java
@@ -0,0 +1,25 @@
+package com.seabornlee.springboot.memberservice.util.enumeration;
+
+public enum DataSourceEnum {
+    ESpirit(1);
+
+    private int value;
+
+    DataSourceEnum(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public DataSourceEnum valueOf(int value){
+        DataSourceEnum[] enums = DataSourceEnum.values();
+        for(DataSourceEnum dataSourceEnum : enums){
+            if(dataSourceEnum.getValue() == value){
+                return dataSourceEnum;
+            }
+        }
+        return null;
+    }
+}
--- src/main/resources/db/migration/V1__20180709_create_table_data_sync_record.sql
+++ src/main/resources/db/migration/V1__20180709_create_table_data_sync_record.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS `data_sync_record`;
 CREATE TABLE `data_sync_record` (
 `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
-`data_source` varchar(50) NULL COMMENT '数据来源，e精灵',
+`data_source` int NULL default 1 COMMENT '数据来源: 1 e精灵',
 `url` varchar(300) NULL COMMENT '请求链接',
 `method` varchar(20) NULL COMMENT 'post/get',
 `parameters` text NULL COMMENT '参数，json',
 `operator` varchar(255) NULL COMMENT '操作员',
 `op_time` long NULL COMMENT '操作时间',
 `is_success` boolean NULL COMMENT '是否成功',
-`status` int 0 COMMENT '任务执行状态:0 已经提交; 1 执行中; 2 完成',
+`status` int NULL default 0 COMMENT '任务执行状态:0 已经提交; 1 执行中; 2 完成',
 PRIMARY KEY (`id`)
 ); ENGINE=InnoDB DEFAULT CHARSET=utf8;
